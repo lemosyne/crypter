dafd89b6a6c88c1192e39a1d4249fd929d56cd4b
Eugene Chou <euchou@ucsc.edu>
2023-04-28 01:03:24 -0700

    feature-gating OpenSSL

--- src/lib.rs
+++ src/lib.rs
@@ -1,125 +1,80 @@
-use openssl::{cipher::Cipher, cipher_ctx::CipherCtx, error};
-use paste::paste;
-use thiserror::Error;
-
-pub type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error(transparent)]
-    Internal(#[from] error::ErrorStack),
-}
-
 pub trait Crypter {
+    type Error;
+
     fn key_length() -> usize;
 
     fn iv_length() -> usize;
 
-    fn encrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>>;
+    fn encrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error>;
 
-    fn onetime_encrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>>;
+    fn onetime_encrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error>;
 
-    fn decrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>>;
+    fn decrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error>;
 
-    fn onetime_decrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>>;
+    fn onetime_decrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error>;
 }
 
-macro_rules! crypter_bulk_impl {
-    ($($crypter:ident),*$(,)?) => {
-        $(paste! {
-            pub struct [<$crypter:camel>];
+#[cfg(feature = "openssl")]
+mod openssl {
+    use crate::Crypter;
+    use openssl::{cipher::Cipher, cipher_ctx::CipherCtx, error::ErrorStack};
+    use paste::paste;
 
-            impl Crypter for [<$crypter:camel>] {
-                fn key_length() -> usize {
-                    Cipher::$crypter().key_length()
-                }
-
-                fn iv_length() -> usize {
-                    Cipher::$crypter().iv_length()
-                }
-
-                fn encrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>> {
-                    let cipher = Cipher::$crypter();
-                    let mut ctx = CipherCtx::new()?;
-                    ctx.encrypt_init(Some(cipher), Some(key), Some(iv))?;
-
-                    let mut output = Vec::with_capacity(data.len());
-                    ctx.cipher_update_vec(data, &mut output)?;
-                    ctx.cipher_final_vec(&mut output)?;
-
-                    Ok(output)
-                }
-
-                fn onetime_encrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>> {
-                    let iv = vec![0; Self::iv_length()];
-                    Self::encrypt(key, &iv, data)
-                }
-
-                fn decrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>> {
-                    let cipher = Cipher::$crypter();
-                    let mut ctx = CipherCtx::new()?;
-                    ctx.decrypt_init(Some(cipher), Some(key), Some(iv))?;
-
-                    let mut output = Vec::with_capacity(data.len());
-                    ctx.cipher_update_vec(data, &mut output)?;
-                    ctx.cipher_final_vec(&mut output)?;
-
-                    Ok(output)
-                }
-
-                fn onetime_decrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>> {
-                    let iv = vec![0; Self::iv_length()];
-                    Self::decrypt(key, &iv, data)
-                }
-            }
-        })*
-    };
-}
-
-crypter_bulk_impl!(
-    aes_128_cbc,
-    aes_128_ctr,
-    aes_128_xts,
-    aes_128_ofb,
-    aes_256_cbc,
-    aes_256_ctr,
-    aes_256_ofb,
-);
-
-pub mod prelude {
-    pub use crate::*;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use anyhow::Result;
-    use rand::{thread_rng, RngCore};
-
-    macro_rules! crypter_test_bulk_impl {
+    macro_rules! crypter_bulk_impl {
         ($($crypter:ident),*$(,)?) => {
             $(paste! {
-                #[test]
-                fn $crypter() -> Result<()> {
-                    let mut key = vec![0; [<$crypter:camel>]::key_length()];
-                    thread_rng().fill_bytes(&mut key);
+                pub struct [<$crypter:camel>];
 
-                    let mut iv = vec![0; [<$crypter:camel>]::iv_length()];
-                    thread_rng().fill_bytes(&mut iv);
+                impl Crypter for [<$crypter:camel>] {
+                    type Error = ErrorStack;
 
-                    let pt = b"this is a super secret message";
-                    let ct = [<$crypter:camel>]::encrypt(&key, &iv, pt)?;
-                    let xt = [<$crypter:camel>]::decrypt(&key, &iv, &ct)?;
+                    fn key_length() -> usize {
+                        Cipher::$crypter().key_length()
+                    }
 
-                    assert_eq!(&pt[..], &xt[..]);
+                    fn iv_length() -> usize {
+                        Cipher::$crypter().iv_length()
+                    }
 
-                    Ok(())
+                    fn encrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error> {
+                        let cipher = Cipher::$crypter();
+                        let mut ctx = CipherCtx::new()?;
+                        ctx.encrypt_init(Some(cipher), Some(key), Some(iv))?;
+
+                        let mut output = Vec::with_capacity(data.len());
+                        ctx.cipher_update_vec(data, &mut output)?;
+                        ctx.cipher_final_vec(&mut output)?;
+
+                        Ok(output)
+                    }
+
+                    fn onetime_encrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error> {
+                        let iv = vec![0; Self::iv_length()];
+                        Self::encrypt(key, &iv, data)
+                    }
+
+                    fn decrypt(key: &[u8], iv: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error> {
+                        let cipher = Cipher::$crypter();
+                        let mut ctx = CipherCtx::new()?;
+                        ctx.decrypt_init(Some(cipher), Some(key), Some(iv))?;
+
+                        let mut output = Vec::with_capacity(data.len());
+                        ctx.cipher_update_vec(data, &mut output)?;
+                        ctx.cipher_final_vec(&mut output)?;
+
+                        Ok(output)
+                    }
+
+                    fn onetime_decrypt(key: &[u8], data: &[u8]) -> Result<Vec<u8>, Self::Error> {
+                        let iv = vec![0; Self::iv_length()];
+                        Self::decrypt(key, &iv, data)
+                    }
                 }
             })*
         };
     }
 
-    crypter_test_bulk_impl!(
+    crypter_bulk_impl!(
         aes_128_cbc,
         aes_128_ctr,
         aes_128_xts,
@@ -128,4 +83,48 @@ mod tests {
         aes_256_ctr,
         aes_256_ofb,
     );
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "openssl")]
+    mod openssl {
+        use crate::{openssl::*, *};
+        use anyhow::Result;
+        use paste::paste;
+        use rand::{thread_rng, RngCore};
+
+        macro_rules! crypter_test_bulk_impl {
+            ($($crypter:ident),*$(,)?) => {
+                $(paste! {
+                    #[test]
+                    fn $crypter() -> Result<()> {
+                        let mut key = vec![0; [<$crypter:camel>]::key_length()];
+                        thread_rng().fill_bytes(&mut key);
+
+                        let mut iv = vec![0; [<$crypter:camel>]::iv_length()];
+                        thread_rng().fill_bytes(&mut iv);
+
+                        let pt = b"this is a super secret message";
+                        let ct = [<$crypter:camel>]::encrypt(&key, &iv, pt)?;
+                        let xt = [<$crypter:camel>]::decrypt(&key, &iv, &ct)?;
+
+                        assert_eq!(&pt[..], &xt[..]);
+
+                        Ok(())
+                    }
+                })*
+            };
+        }
+
+        crypter_test_bulk_impl!(
+            aes_128_cbc,
+            aes_128_ctr,
+            aes_128_xts,
+            aes_128_ofb,
+            aes_256_cbc,
+            aes_256_ctr,
+            aes_256_ofb,
+        );
+    }
 }
